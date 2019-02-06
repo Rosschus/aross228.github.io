1d2f3ad75f8fb2d39b17edaf9924bc5bf1a48487
ARoss228 <seh_hes@mail.ru>
2019-02-07 03:05:16 +0500

    'upd1'

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <? 
 $access_key = 'XuIFd5dS5XS1MshYoUEG';
-file_get_contents('https://api.vk.com/method/showAppWidgetPreviewBox?type=table&return { 
-    "title": "Цитата", 
-    "text": "Текст цитаты" 
-};');
+file_get_contents('https://api.vk.com/method/showGroupSettingsBox');
 
 
 
